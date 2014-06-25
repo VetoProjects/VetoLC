9d3564b5ee250d6ffbc963fdeb80a57c7f919cb5
Veit Heller <s0539501@htw-berlin.de>
2014-06-25 13:33:31 +0200

    Updated default.glsl

--- default.glsl
+++ default.glsl
@@ -3,36 +3,39 @@
 
 in vec2  uv;
 in float time;
-// uniform float[1764] audioData;
-uniform sampler1D audioData;
+uniform sampler1D audioLeftData;
+uniform sampler1D audioRightData;
 
-//uniform highp vec2 c = vec2(.1,.1);
 uniform int iter = 100;
 out vec4 color;
 
-float aud(float val){ return texture(audioData, val).r ; }
+float left (float val){ return texture(audioLeftData , val).r ; }
+float right(float val){ return texture(audioRightData, val).r ; }
+
+float clamp(float val){ return clamp(val, 0.0, 1.0); }
+vec2  clamp(vec2  val){ return clamp(val, 0.0, 1.0); }
+vec3  clamp(vec3  val){ return clamp(val, 0.0, 1.0); }
+vec4  clamp(vec4  val){ return clamp(val, 0.0, 1.0); }
 
 void main() {
+	int i;
+	float x, y, d, v, l, r, intensity, audioVal;
+	vec2 p, c, z;	
+
 	color = vec4(1);
-	vec2 p = 1.0 - uv * 2.0;
-	float audioVal = aud(uv.x) / 3;
-
-// Star
-//	if(length(p) + sin(atan(p.y, p.x)*5)/3 < .5)
-//		return;
-
+	p = uv * 2.0 - 1.0;
+	audioVal = (left(uv.x) + right(uv.x)) / 6;
 
 // Fractal inner
 	color = vec4(0, audioVal, 0, 1);
 
 // Fractal
-	vec2 z = p * vec2(1.2,1.2);
-	vec2 c = vec2(cos(time/1200.0),sin(time/1000.0));
-//	vec2 c = vec2(sin(time / 1000.0), cos(time / 500.0));
-	int i;
+//*
+	z = p * vec2(1.2,1.2);
+	c = vec2(cos(time/1200.0),sin(time/1000.0));
 	for(i=0; i < iter; ++i) {
-		float x = (z.x * z.x - z.y * z.y) + c.x + aud(c.x) / 5;
-		float y = (z.x * z.y + z.x * z.y) + c.y + aud(c.y) / 5;
+		x = (z.x * z.x - z.y * z.y) + c.x + left(c.x) / 5;
+		y = (z.x * z.y + z.x * z.y) + c.y + left(c.y) / 5;
 
 		if((x * x + y * y) > 4.0)
 			break;
@@ -40,29 +43,56 @@ void main() {
 		z.y = y;
 	}
 	if(i < iter){
-		color.b = clamp(float(i) * 5.0 / float(iter), 0, 1);
-		color.g =clamp((float(i) * 5.0 / float(iter) - .3) / .7, 0, 1);
-		color.r = clamp((float(i) * 5.0 / float(iter) - .7) / .3, 0, 1);
+		color.b = clamp( float(i) * 5.0 / float(iter)			);
+		color.g = clamp((float(i) * 5.0 / float(iter) - .3) / .7);
+		color.r = clamp((float(i) * 5.0 / float(iter) - .7) / .3);
 	}
+//*/
 
-
-// Wave line
+//* Wave line
+//	audioVal = abs(audioVal);
 	if(p.y < max(0, audioVal) && p.y > min(0, audioVal)){
-		float intensity = abs(p.y - audioVal/2) / abs(audioVal) * 2;
+		intensity = abs(p.y - audioVal / 2) / abs(audioVal) * 2;
 		color.rgb *= intensity;
-		color.rgb += 1-intensity - intensity* vec3(2.333, 0.428571429, 0);
+		color.rgb += clamp(1 - intensity - intensity * vec3(-.2, .8, 2.333));
 		
 	}
 
 // Circle
 /*
-	c = p + vec2(sin(time/1000), cos(time/1000));
-	float d = length(c);
-	float v = aud(abs(atan(c.y, c.x) / 3.141592)) / 10 + .4;
-	if(d < v){
-		float intensity = length(c) / v;
+	c = vec2(sin(time/1000), cos(time/1000)) * .7 - p;
+	d = length(c);
+	v = left(abs(atan(c.y, c.x) / 3.141592)) / 3;
+	if(d < .2 && d + v >= .2 || d > .2 && d + v <= .2){
+		intensity = abs(d  + v/2 - .2) / abs(v) * 2;
 		color.rgb *= intensity;
-		color.rgb += 1 - intensity - intensity * vec3(1, 0, .5);
-		return;
-	}*/
+		color.rgb += clamp(1 - intensity - intensity * vec3(1, 0, .5));
+	}
+//*/
+
+
+/* Left
+	l = abs(left(uv.y)/2.0);
+	if(l >= uv.x){
+		float intensity = (uv.x - l / 2) / abs(l) * 2;
+		color.rgb *= intensity;
+		color.rgb += clamp((1 - intensity) * vec3(1,0,0));
+	}
+//*
+
+
+/* Right
+	r = abs(right(uv.y)/2.0);
+	if(abs(r/2.0) >= 1-uv.x){
+		intensity = clamp((1 - uv.x - r / 2) / abs(r) * 2, 0, 1);
+		color.rgb *= intensity;
+		color.rgb += (1 - intensity) * clamp(vec3(1,0,0));
+	}
+//*/
+
+// Star
+/*
+	if(length(p) + sin(atan(p.y, p.x)*5 + time/100)/3 < .5)
+		color = vec4(1);
+//*/
 }
